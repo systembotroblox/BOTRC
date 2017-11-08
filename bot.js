42272ba2ada1304dfdfd229ba9451e44c2ef9d88
systembotroblox <32761646+systembotroblox@users.noreply.github.com>
2017-11-08 22:30:02 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,7 @@ client.on('guildMemberAdd', member => {
        member.user.send(`Welcome to the server, ${member}!`);
        member.guild.defaultChannel.send(`Welcome to the server, ${member}!`);
        console.log(`${member.user.username} has joined`);
+       member.addRole('340748473421004801').catch(console.error);
 });
 
 client.login(process.env.BOT_TOKEN);
