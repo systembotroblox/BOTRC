218d58f32fbd72310b35fcded877fe65fb238d5c
systembotroblox <32761646+systembotroblox@users.noreply.github.com>
2017-11-25 23:25:13 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setGame('ROBLOX', 'https://www.twitch.tv/roblox');
 });
 
 client.on('message', message => {
@@ -13,9 +12,6 @@ client.on('message', message => {
 });
 
 client.on('guildMemberAdd', member => {
-       member.user.send(`Welcome to the server, ${member}!`);
-       member.guild.defaultChannel.send(`Welcome to the server, ${member}!`);
-       console.log(`${member.user.username} has joined`);
        member.addRole('340748473421004801').catch(console.error);
 });
 
